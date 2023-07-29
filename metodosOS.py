5348f2e92ec2db3341b8447631e33f5dcae12199
JunioResende <junioresende23@gmail.com>
2023-07-29 13:18:51 -0300

    metodos

--- metodosOS.py
+++ metodosOS.py
@@ -1,1 +1,17 @@
+import os
+from time import sleep
 
+print('caminho ate aqui')
+caminho = os.getcwd()
+print(caminho)
+
+print('listar arquivos dentro de test folder')
+listFiles = os.listdir()
+print(listFiles)
+
+print('criando pasta testFolder2')
+os.mkdir('testFolder2')
+print(listFiles)
+sleep(10)
+# remove testFolder2
+os.rmdir('testFolder2')
